fc0b9392e0905fad6cfd099445220c83b997bf3b
TIjDG <uhbe45255@gmail.com>
2022-08-31 20:44:32 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
 
-    const api_key = "KvqzveEkuR_W7ofppgIv3pETQrXC3vyg"
+    const api_key = "RzgkFod7XazlIrcnEHAUPO8KmRJ1odax"
 
 
 
@@ -45,7 +45,7 @@ const Home = () => {
         method: 'GET'
       };
 
-      const api_key = "KvqzveEkuR_W7ofppgIv3pETQrXC3vyg"
+      const api_key = "RzgkFod7XazlIrcnEHAUPO8KmRJ1odax"
       // const api_key = ""
       
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
